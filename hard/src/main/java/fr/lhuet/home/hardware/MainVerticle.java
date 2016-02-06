10d6ea4d2e1263261cbf7e287fc7f9943b45c596
Laurent HUET <laurent.huet@gmail.com>
2016-02-07 00:18:18 +0100

    add main method to test Verticle

--- hard/src/main/java/fr/lhuet/home/hardware/MainVerticle.java
+++ hard/src/main/java/fr/lhuet/home/hardware/MainVerticle.java
@@ -2,11 +2,22 @@ package fr.lhuet.home.hardware;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 
 /**
  * Created by lhuet on 31/12/15.
  */
 public class MainVerticle extends AbstractVerticle {
+
+    public static void main(String[] args) {
+        JsonObject conf = new JsonObject();
+        conf.put("dhw-system.dhw", Vertx.class.getClassLoader().getResource("w1-dhw.data").getPath());
+        conf.put("dhw-system.buffer", Vertx.class.getClassLoader().getResource("w1-buffer.data").getPath());
+        DeploymentOptions options = new DeploymentOptions().setConfig(conf);
+        Vertx.vertx().deployVerticle(MainVerticle.class.getName(), options);
+    }
+
 
     @Override
     public void start() throws Exception {
