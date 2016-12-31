a5f989a7b64542d19cf5eda9a2489ce3a4e16139
Laurent HUET <laurent.huet@gmail.com>
2016-12-31 15:28:30 +0100

    fix webapp scripts

--- web/src/main/dist/bin/start.sh
+++ web/src/main/dist/bin/start.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-# Load DeviceTree beaglebone "module"
-# echo BB-W1 > /sys/devices/bone_capemgr.8/slots
-# echo BB-UART1 > /sys/devices/bone_capemgr.8/slots
 
-export JAVA_OPTS=-Djava.library.path=/usr/lib/jni
-
-cd /home/debian/vertx3/hard-1.0-SNAPSHOT
-./bin/hard run --cluster --conf ../conf.json fr.lhuet.home.hardware.MainVerticle
+cd /home/debian/vertx3/web-1.0-SNAPSHOT
+./bin/web start --cluster --vertx-id domoweb fr.lhuet.home.domoweb.WebappVerticle
 
