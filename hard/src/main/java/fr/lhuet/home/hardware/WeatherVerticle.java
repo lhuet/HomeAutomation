12d560b0c458e546c51a46a339e4dd187d8662b3
Laurent HUET <laurent.huet@gmail.com>
2016-12-31 15:08:37 +0100

    index bmp85 measures to elasticsearch

--- hard/src/main/java/fr/lhuet/home/hardware/WeatherVerticle.java
+++ hard/src/main/java/fr/lhuet/home/hardware/WeatherVerticle.java
@@ -1,20 +1,32 @@
 package fr.lhuet.home.hardware;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+
+import java.time.Instant;
 
 /**
  * Created by lhuet on 31/12/16.
  */
 public class WeatherVerticle extends AbstractVerticle{
 
+
     private static Logger logger = LoggerFactory.getLogger(WeatherVerticle.class);
+
+    private static final String ES_TYPE_DOC = "record";
+    private static final String ES_INDEX = "weather";
 
     private String bmp085LinuxVirtualFsDir;
     private float bmp085Temp;
     private float bmp085Pressure;
+
+
+
 
     @Override
     public void start() throws Exception {
@@ -23,15 +35,23 @@ public class WeatherVerticle extends AbstractVerticle{
 
         if (bmp085LinuxVirtualFsDir != null) {
 
-            vertx.setPeriodic(5000, res -> {
-               readBmp085();
+            // Read sensor on startup
+            readBmp085(event -> {
+                indexToES();
+            });
+
+            // ... and then every minute
+            vertx.setPeriodic(60000, res -> {
+               readBmp085(event -> {
+                   indexToES();
+               });
             });
 
         }
 
     }
 
-    private void readBmp085() {
+    private void readBmp085(Handler<AsyncResult> ar) {
 
         Future<Void> startFuture = Future.future();
 
@@ -60,6 +80,31 @@ public class WeatherVerticle extends AbstractVerticle{
 
         }, startFuture);
 
+        ar.handle(startFuture);
+
+    }
+
+
+    private void indexToES() {
+
+        JsonObject docToindex = new JsonObject()
+                .put("timestamp", Instant.now())
+                .put("tempCave", this.bmp085Temp)
+                .put("pressure", this.bmp085Pressure);
+        JsonObject message = new JsonObject()
+                .put("index", ES_INDEX)
+                .put("type", ES_TYPE_DOC)
+                .put("id", "sensor_" + Instant.now().toEpochMilli())
+                .put("source", docToindex);
+
+        vertx.eventBus().send("elastic.index", message, msg -> {
+            if (msg.succeeded()) {
+                logger.info(msg.result().body());
+            }
+            else {
+                logger.error("indexWheaterIntoES Error ...", msg.cause());
+            }
+        });
 
     }
 
